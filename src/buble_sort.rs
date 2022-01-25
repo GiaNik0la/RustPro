cad1c281d9cff0afd21329c07d9a494568c4babc
GiaNik0la <gianikolishvili@gmail.com>
2022-01-25 07:48:54 -0500

    Littlebit better comments

--- src/buble_sort.rs
+++ src/buble_sort.rs
@@ -1,11 +1,12 @@
 pub fn run(mut test_array: [i32; 6]) {
-    // Getting safe count of array
+    // Getting safe count of array, because if we don't then we will go out of range
     let test_array_len = test_array.len() - 1;
 
     // Looping into test_array until sorting is done
     for _i in 0..test_array.len() {
-        // Sorting loop
+        // Looping in safe length
         for _a in 0..test_array_len {
+            // Swapping elements if one we are on is grater than the next one
             if test_array[_a] > test_array[_a + 1] {
                 let test_save = test_array[_a + 1];
                 test_array[_a + 1] = test_array[_a];
